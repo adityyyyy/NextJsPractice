7628762817a89302143bdfc81a33410b5b50979c
Adi <aditya19patel@gmail.com>
2024-05-31 16:08:17 +0000

    invoice page almost completed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from "@/app/ui/skeletons";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
 
@@ -13,7 +13,9 @@ export default async function Page() {
         Dashboard
       </h1>  
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Cards /> 
+        <Suspense fallback={<CardsSkeleton />}>
+          <Cards /> 
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,4 @@
+'use server';
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -37,7 +38,6 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
-
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
